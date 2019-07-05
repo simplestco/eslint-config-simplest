36a2d2b25397fc68f08c2e4ba35eb7749544c7da
Lord Rasta <adantoscano@gmail.com>
2019-07-05 14:50:10 +0200

    Add Prettier

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = {
-  extends: ['eslint:recommended', 'airbnb'],
+  extends: ['eslint:recommended', 'airbnb', 'prettier'],
   env: {
     mocha: true
   },
-  plugins: ['chai-friendly'],
+  plugins: ['chai-friendly', 'prettier'],
   rules: {
+    'prettier/prettier': ['error', { "singleQuote": true, "printWidth": 200 }],
     'no-unused-expressions': 0,
     'chai-friendly/no-unused-expressions': 2,
     'comma-dangle': 0
