a7c5719c0df6817125bd93c930be042c8a322e06
Lord Rasta <adantoscano@gmail.com>
2019-07-19 14:41:03 +0200

    Add babel-eslint and update dependencies

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ module.exports = {
   env: {
     mocha: true
   },
+  parser: "babel-eslint",
+  "rules": {
+  },
   plugins: ['chai-friendly', 'prettier'],
   rules: {
     'prettier/prettier': ['error', { "singleQuote": true, "printWidth": 200 }],
     'no-unused-expressions': 0,
     'chai-friendly/no-unused-expressions': 2,
-    'comma-dangle': 0
+    'comma-dangle': 0,
+    'react/jsx-filename-extension': 'off'
   }
 };
