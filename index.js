3aed47d3972b2968b136e83cb9adea0571434fe5
Lord Rasta <adantoscano@gmail.com>
2019-07-19 14:42:54 +0200

    Add more rules

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ module.exports = {
     'no-unused-expressions': 0,
     'chai-friendly/no-unused-expressions': 2,
     'comma-dangle': 0,
-    'react/jsx-filename-extension': 'off'
+    'react/jsx-filename-extension': 'off',
+    "object-curly-newline": "off",
+    "consistent-return": "warn",
+    "func-names": "off"
   }
 };
