9ce1dd9eaf768a39b02db974d568e1e50cd6f652
Lord Rasta <adantoscano@gmail.com>
2019-08-27 10:05:45 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ module.exports = {
   env: {
     mocha: true
   },
-  parser: "babel-eslint",
-  "rules": {
+  parser: 'babel-eslint',
+    rules: {
   },
   plugins: ['chai-friendly', 'prettier'],
   rules: {
-    'prettier/prettier': ['error', { "singleQuote": true, "printWidth": 200 }],
+    'prettier/prettier': ['error', { 'singleQuote': true, 'printWidth': 200 }],
     'no-unused-expressions': 0,
     'chai-friendly/no-unused-expressions': 2,
     'comma-dangle': 0,
     'react/jsx-filename-extension': 'off',
-    "object-curly-newline": "off",
-    "consistent-return": "warn",
-    "func-names": "off"
+    'object-curly-newline': 'off',
+    'consistent-return': 'warn',
+    'func-names': 'off'
   }
 };
